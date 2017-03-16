5b977f49e8db941afabdd0984373f826dfe2fa6f
Jonathan Lopez <JonL_2086@hotmail.com>
2017-03-16 00:18:35 -0700

    Finished ViewModels. Working on Views.

---
+++ Assignment2/ViewModels/NewAccountViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using Assignment2.Models;
+
+namespace Assignment2.ViewModels
+{
+    public class NewAccountViewModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public String FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public String LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Email Address")]
+        public String Email { get; set; }
+
+        public int ProgramID { get; set; }
+
+        [Required]
+        [Display(Name = "Program Options")]
+
+        public virtual ICollection<Program> ProgramOptions { get; set; }
+
+        [Required]
+        [Display(Name = "email me program updates")]
+        public bool EmailUpdates { get; set; }
+
+    }
+}
---
+++ Assignment2/ViewModels/PasswordGeneratorViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Assignment2.ViewModels
+{
+    public class PasswordGeneratorViewModel
+    {
+        [Required]
+        [Display(Name = "Last Name")]
+        public String LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Birth Year")]
+        public int BirthYear { get; set; }
+
+        [Required]
+        [Display(Name = "Favorite Color")]
+        public String FavoriteColor { get; set; }
+
+        public String PasswordSuggestion { get; set; }
+    }
+}
